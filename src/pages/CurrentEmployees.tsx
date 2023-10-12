0ced844c39602cc3b1f7a73ed6b7b2000e8f33ca
Cyril BARTZ <bartzcyril@gmail.com>
2023-10-12 15:33:37 +0200

    add fake data

--- src/pages/CurrentEmployees.tsx
+++ src/pages/CurrentEmployees.tsx
@@ -1,7 +1,25 @@
 import {DataTableEmployee} from "../components/DataTable/DataTableEmployee.tsx";
 import {Header} from "../components/Header.tsx";
+import {fakeEmployees} from "../data/fakeEmployee.ts";
+import {useDispatch} from "react-redux";
+import {createEmployee} from "../utils/slice/employeeSlice.ts";
 
 export function CurrentEmployees() {
+
+    const dispatch = useDispatch()
+
+    fakeEmployees.map((e) => dispatch(createEmployee({
+            firstName: e.firstName,
+            lastName: e.lastName,
+            street: e.street,
+            city: e.city,
+            zipCode: e.zipCode,
+            dateOfBirth : new Date(e.dateOfBirth).toLocaleDateString(),
+            startDate: new Date(e.startDate).toLocaleDateString(),
+            selectedOptionState: e.selectedOptionState,
+            selectedOptionDepartment: e.selectedOptionDepartment
+        }))
+    )
 
     return (
         <>

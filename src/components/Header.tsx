a4c84b52cea7e3703a4facf4e80d1754a6b4928c
Cyril BARTZ <bartzcyril@gmail.com>
2023-10-12 15:04:09 +0200

    correction

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,16 +4,16 @@ export function Header() {
     return (
         <header>
             <nav className="navbar navbar-expand-lg navbar-light p-4">
-                <Link to="/" className="navbar-brand">
+                <Link to="/HRnet/" className="navbar-brand">
                     <img src="/img/logo.svg" alt="logo" width={50} height={50}/>
                 </Link>
                 <div>
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <Link to="/vite-react-router/" className="nav-link">Home</Link>
+                            <Link to="/HRnet/" className="nav-link">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="/vite-react-router/employee-list" className="nav-link">View Current Employees</Link>
+                            <Link to="/HRnet/employee-list" className="nav-link">View Current Employees</Link>
                         </li>
                     </ul>
                 </div>

4b6fb89567fdde00b1214b684a3795d6735f58f5
Cyril BARTZ <bartzcyril@gmail.com>
2023-10-12 17:43:35 +0200

    fake data

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,26 @@
 import {Form} from "../components/Form/Form.tsx";
 import {Header} from "../components/Header.tsx";
+import {useDispatch} from "react-redux";
+import {createEmployee} from "../utils/slice/employeeSlice.ts";
+import {fakeEmployees} from "../data/fakeEmployee.ts";
 
 export function App() {
+
+    const dispatch = useDispatch()
+
+    for (let i= 0; i < 1000; i++) {
+        dispatch(createEmployee({
+            firstName: fakeEmployees[0].firstName,
+            lastName: fakeEmployees[0].lastName,
+            street: fakeEmployees[0].street,
+            city: fakeEmployees[0].city,
+            zipCode: fakeEmployees[0].zipCode,
+            dateOfBirth : new Date(fakeEmployees[0].dateOfBirth).toLocaleDateString(),
+            startDate: new Date(fakeEmployees[0].startDate).toLocaleDateString(),
+            selectedOptionState: fakeEmployees[0].selectedOptionState,
+            selectedOptionDepartment: fakeEmployees[0].selectedOptionDepartment
+        }))
+    }
 
   return (
       <>
